d885e3b0418ff6320236a580e1a63b77ce0aeb43
ethaan <ethan.rosanoo@gmail.com>
2017-04-26 23:48:58 -0500

    Adding build script

--- config/index.js
+++ config/index.js
@@ -3,7 +3,15 @@ import graphql from '../data';
 
 export default {
   build: {
+    index: path.resolve(__dirname, '../dist/index.html'),
     outputPath: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    productionGzip: false,
+    assetsPublicPath: '/',
+    assetsSubDirectory: 'static',
+    productionSourceMap: true,
+    bundleAnalyzerReport: process.env.npm_config_report,
+    productionGzipExtensions: ['js', 'css'],
   },
   dev: {
     env: "'development'",
